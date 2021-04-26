c387da17560ff02512e978261d8735f8f8237f7e
changlin93 <767425412@qq.com>
2021-04-27 07:54:02 +0800

    feat(deps): 配置项目规范
    配置eslint、prettier、husky、lint-staged、editorconfig

---
+++ README.md
@@ -0,0 +1,6 @@
+安装 prettier 之后可以使用命令：
+
+```shell
+// . 表示格式化所有文件
+$ npx prettier --write .
+```
---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<el-button @click="getData">button</el-button>
+		<div v-if="source">
+			<span>{{ source.name }}</span>
+			<p class="item">
+				<span>头像：</span>
+				<img :src="source.avatar_url" alt="" />
+			</p>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import axios from '@/utils/axios'
+
+export default defineComponent({
+	name: 'home',
+	setup() {
+		const source = ref(null)
+		const getData = async () => {
+			const result = await axios.get('/users/changlin93')
+			source.value = result.data
+		}
+
+		return {
+			getData,
+			source
+		}
+	}
+})
+</script>
+
+<style lang="scss" scoped>
+img {
+	width: 60px;
+	height: 60px;
+	border-radius: 50%;
+	overflow: hidden;
+}
+</style>
